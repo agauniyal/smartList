b81842cc49b191caab94f9d9ce06146eceae78dd
agauniyal <agauniyal@outlook.com>
2017-10-14 02:23:39 +0530

    Add include folder

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "include/smartList.hpp"
+
+using std::cout;
+
+int main()
+{
+    SL<int> list;
+    list.insert({ 1, 2, 3, 4, 5 });
+
+    const auto print = [](const auto N) { cout << N << ' '; };
+    const auto mul10 = [](auto &num) { num *= 10; };
+
+    list.forEach(mul10);
+    list.forEach(print);
+    cout << '\n';
+
+    list.reverse();
+    list.forEach(print);
+    cout << '\n';
+}
